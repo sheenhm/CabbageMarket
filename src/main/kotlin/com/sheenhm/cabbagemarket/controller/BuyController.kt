8a7f91b74156afe0f3e88ad89cd26929cf4c31d3
sheenhm <sheenhm@hanyang.ac.kr>
2023-05-16 15:53:48 +0900

    2023. 5. 16. (TUE)

--- src/main/kotlin/com/sheenhm/cabbagemarket/controller/BuyController.kt
+++ src/main/kotlin/com/sheenhm/cabbagemarket/controller/BuyController.kt
@@ -1,8 +1,6 @@
 package com.sheenhm.cabbagemarket.controller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class BuyController {
--- src/main/kotlin/com/sheenhm/cabbagemarket/controller/MyInfoController.kt
+++ src/main/kotlin/com/sheenhm/cabbagemarket/controller/MyInfoController.kt
@@ -1,10 +1,13 @@
 package com.sheenhm.cabbagemarket.controller
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 class MyInfoController {
     data class MyInfo(
+        val userId: String,
         val orderList: List<Int>,
         val geo: Geo,
         val name: String,
@@ -16,13 +19,29 @@ class MyInfoController {
         val y: Double
     )
 
+    private val myInfo = mutableListOf<MyInfo>()
+
     @GetMapping("/myinfo/{userId}")
-    fun getMyInfo(@PathVariable userId: String): MyInfo {
-        return MyInfo(
-            listOf(1, 2, 3, 4),
-            Geo(128.028737474, 38.2393462873),
-            "",
-            ""
+    fun getMyInfo(@PathVariable userId: String): MyInfo? {
+        return myInfo.find { it.userId == userId }
+    }
+
+    @PutMapping("/myinfo/{userId}/edit")
+    fun editMyInfo(@PathVariable userId: String, @RequestBody request: MyInfo): ResponseEntity<String> {
+        val existingInfo = myInfo.find { it.userId == userId }
+            ?: return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found")
+
+        val updatedInfo = MyInfo(
+            userId = userId,
+            orderList = existingInfo.orderList,
+            geo = Geo(request.geo.x, request.geo.y),
+            name = request.name,
+            tel = request.tel
         )
+
+        myInfo.remove(existingInfo)
+        myInfo.add(updatedInfo)
+
+        return ResponseEntity.ok().body("{\"status\": \"ok\"}")
     }
 }
